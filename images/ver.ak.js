98c09025fdbfff1906fd8b81459e5ae6d662e5c5
theK <rain5x@naver.com>
2020-03-15 02:21:42 +0900

    v2.0.6

---
+++ images/ver.ak.js
@@ -0,0 +1,20 @@
+// document.write(dependenciesVer.IcoMoon);
+//
+var blogInfo = {
+	"name": "AneOK",
+	"logoVer": "v2", //공개
+	"Build": "20", //유지 Build or Maintenance Version
+	"version": "2.0.6", //일반적인
+	"startDate": "2019. 4. 29",
+	"update_startDate": "2019. 12. 03",
+	"update": "2020. 03. 09"
+};
+var dependenciesVer = {
+	"jquery": "3.4.1",
+	"Bootstrap": "4.4.1 somePart",
+	"popper.js": "1.14.7",
+	"iconFont": "95",
+	"DynamicScrollspy": "0.2.0",
+	"highlight.js": "9.18.1"
+};
+var updated = ""; // 업데이트 날짜 들어갈 문구 
